26090af500241c462c73a69e7e7beef77e56fbb2
Venkata Murali Krishna Vemula <muralikrish.vemula@gmail.com>
2026-03-13 06:56:51 +0000

    added delivery team seeds file

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -17,9 +17,12 @@ select
     p.category,
     p.productname,
     p.subcategory,
-    {{ markup('ordersellingprice', 'ordercostprice') }} as markup
+    {{ markup('ordersellingprice', 'ordercostprice') }} as markup,
+    d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} p
 on o.productid = p.productid
+left join {{ ref('delivery_team') }} d 
+on o.shipmode = d.shipmode
